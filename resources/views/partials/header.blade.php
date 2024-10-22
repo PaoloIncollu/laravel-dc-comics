7d0e44756e8f25cc7f7577549d2a3679c90a44ad
PaoloIncollu <167555333+PaoloIncollu@users.noreply.github.com>
2024-10-22 15:29:33 +0200

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+@php
+    $links = [
+        [
+            'url' => '/',
+            'label' => 'Home',
+            'active' => true,
+        ],
+        [
+            'url' => '/chi-siamo',
+            'label' => 'Chi siamo',
+            'active' => true,
+        ],
+        [
+            'url' => '/contatti',
+            'label' => 'Contatti',
+            'active' => false,
+        ],
+    ];
+@endphp
+
+<header>
+    <section class="Gaetano">
+        <h1>
+            Gaetano Venuto
+        </h1>
+    </section>
+    
+</header>
